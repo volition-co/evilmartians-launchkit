001ac3f65e290117001702c76a51e30f285950aa
Anton Lovchikov <antiflasher@gmail.com>
2025-05-22 16:14:25 +0100

    Refactor buttons

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 function handleMobileNav() {
-  const navButton = document.querySelector('.nav-button');
-  const navMenu = document.querySelector('.nav-menu');
+  const navButton = document.querySelector("#nav-button");
+  const navMenu = document.querySelector(".nav-menu");
 
-  navButton.addEventListener('click', () => {
-    navMenu.classList.toggle('open');
-    navButton.classList.toggle('active');
+  navButton.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+    navButton.classList.toggle("active");
   });
 
-  document.documentElement.addEventListener('click', (event) => {
+  document.documentElement.addEventListener("click", (event) => {
     if (!navButton.contains(event.target) && !navMenu.contains(event.target)) {
-      navMenu.classList.remove('open');
-      navButton.classList.remove('active');
+      navMenu.classList.remove("open");
+      navButton.classList.remove("active");
     }
   });
 }
